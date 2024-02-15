a70a2c68ff0fb0d4ad0870af2eea5b5a6072c0ef
zhiquan <zhiquan96@gmail.com>
2024-02-15 17:37:24 +0000

    modify is_pinned_by_king

--- ZQ/moves.py
+++ ZQ/moves.py
@@ -258,16 +258,6 @@ class UniversalMovementValidation:
     def is_pinned_to_own_king(
         piece: Piece, board: Dict[Tuple[int, int], Piece], new_x: int, new_y: int
     ) -> bool:
-
-        color = piece.color
-
-        king_x, king_y = KingValidation.find_king_position(board, color)
-
-        # Determine the direction vector from the king to the piece being moved
-        dx, dy = BoardUtils.get_direction_vector_from_king(
-            piece=piece, king_x=king_x, king_y=king_y
-        )
-
         # Make a copy of the original board to simulate the move
         simulated_board = deepcopy(board)
 
@@ -276,55 +266,54 @@ class UniversalMovementValidation:
             simulated_board=simulated_board, piece=piece, new_x=new_x, new_y=new_y
         )
 
-        # Iterate from the direction of the king on the simulated board to check for potential pins
-        x, y = piece.x + dx, piece.y + dy
-        while UniversalMovementValidation.is_within_board(x, y):
-            piece_at_position = simulated_board.get((x, y))
+        color = piece.color
 
-            if UniversalMovementValidation.is_not_occupied_by_allies(
-                simulated_board, x, y, color
-            ):
-
-                piece_at_position = board.get((x, y))
-
-                # If it returns true, it can either be the opposing color or empty.
-                # So your check should be if it is not empty
-
-                # if its the opposing color
-                if piece_at_position.type != PieceType.EMPTY:
-
-                    if BoardUtils.is_in_direct_contact_with_opposing_piece(
-                        piece_at_position=piece_at_position, dx=dx, dy=dy
-                    ):
-                        return True
-
-                    else:
-                        break
-                else:
-                    x += dx
-                    y += dy
-
-            else:
-                break
-
-        return False
-
-    @staticmethod
-    def is_king_in_check_after_king_move(
-        board: Dict[Tuple[int, int], Piece], king_piece: Piece, new_x: int, new_y: int
-    ) -> bool:
-        # simulate new board
-        simulated_board = deepcopy(board)
-        # Simulate the move of the piece on the simulated board
-        BoardUtils.simulate_piece_move(
-            simulated_board=simulated_board, piece=king_piece, new_x=new_x, new_y=new_y
+        king_x, king_y = KingValidation.find_king_position(board, color)
+        simulated_king_x, simulated_king_y = KingValidation.find_king_position(
+            simulated_board, color
         )
 
-        # Check all directions, horizontal, vertical, diagonal for direct contact with opposing queen, bishop and
-        # this doesnt work because it doesn't cover moving to where a knight or pawn covers a square.
-        # need to have a list of available moves after each move
+        # A piece other than the king has moved
+        if king_x == simulated_king_x and king_y == simulated_king_y:
+            # Determine the direction vector from the king to the piece being moved
+            dx, dy = BoardUtils.get_direction_vector_from_king(
+                piece=piece, king_x=king_x, king_y=king_y
+            )
 
-        pass
+            # Iterate from the direction of the king on the simulated board to check for potential pins
+            x, y = piece.x + dx, piece.y + dy
+            while UniversalMovementValidation.is_within_board(x, y):
+                piece_at_position = simulated_board.get((x, y))
+
+                if UniversalMovementValidation.is_not_occupied_by_allies(
+                    simulated_board, x, y, color
+                ):
+
+                    piece_at_position = simulated_board.get((x, y))
+
+                    # If it returns true, it can either be the opposing color or empty.
+                    # So your check should be if it is not empty
+
+                    # if its the opposing color
+                    if piece_at_position.type != PieceType.EMPTY:
+
+                        if BoardUtils.is_in_direct_contact_with_opposing_piece(
+                            piece_at_position=piece_at_position, dx=dx, dy=dy
+                        ):
+                            return True
+
+                        else:
+                            break
+                    else:
+                        x += dx
+                        y += dy
+
+                else:
+                    break
+
+        # Implement for when king itself has moved
+
+        return False
 
     @staticmethod
     def is_occupied_by_opposing(
