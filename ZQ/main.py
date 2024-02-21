4a93a7018899fc2cace49e3bf838f7c544254eac
zhiquan <zhiquan96@gmail.com>
2024-02-21 13:23:29 +0000

    add coverage reporting

--- ZQ/main.py
+++ ZQ/main.py
@@ -13,26 +13,26 @@ from utility import BoardUtils
 from pieces import FEN_MAP
 
 # clean up imports in the final stage
-
-# Infinite loop
-board = Board()
-starting_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
-board.process_fen(starting_fen)
-print(board)
-
-# Gameloop
-while not board.king_in_checkmate:
-    notation = Notation.get_valid_notation(board.board)
-    board.move_piece(notation)
+if __name__ == "__main__":
+    # Infinite loop
+    board = Board()
+    starting_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+    board.process_fen(starting_fen)
     print(board)
 
-    if board.king_in_checkmate == True:
-        break
+    # Gameloop
+    while not board.king_in_checkmate:
+        notation = Notation.get_valid_notation(board.board)
+        board.move_piece(notation)
+        print(board)
+
+        if board.king_in_checkmate == True:
+            break
 
 # check for checkmate
 # checkmate is check + no valid moves available
 # current way of checking for check does not cover situation whereby 2 pieces give checks at once
-q
+
 # check if king is in check, available moves are to move it out of check
 # move king itself or use other piece to block
 # reverse translator from grid to notation, and if invalid move is played, show valid_moves_list
